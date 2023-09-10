0e37a39536dbffb6e3492e96da02eeee0cfcd802
Hassan <hakber134@gmail.com>
2023-09-09 20:39:42 -0600

    working on redux reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Contact from './components/Contact/Contact';
-import Login from './components/Login/Login';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user";
 
 function App() {
-  return <Router>
-    <Header />
-    <Routes>
-      <Route path ="/" element={<Home />} />
-      <Route path = "/contact" element={<Contact />} />
-      <Route path = "/account" element={<Login />} />
-    </Routes>
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/account" element={<Login />} />
+      </Routes>
 
-    <Footer />
-  </Router>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
