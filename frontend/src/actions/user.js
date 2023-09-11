f0ea04e065b42e1b19b77ebb724da2606b20411c
Hassan <hakber134@gmail.com>
2023-09-10 21:30:55 -0600

    Backend admin needs tweaking

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -172,3 +172,31 @@ export const deleteProject = (id) => async (dispatch) => {
     });
   }
 };
+
+export const contactUs = (name, email, message) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "CONTACT_US_REQUEST",
+    });
+
+    const { data } = await axios.post(
+      "/api/v1/contact",
+      { name, email, message },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch({
+      type: "CONTACT_US_SUCCESS",
+      payload: data.message,
+    });
+  } catch (error) {
+    dispatch({
+      type: "CONTACT_US_FAILURE",
+      payload: error.response.data.message,
+    });
+  }
+};
